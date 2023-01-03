d11c65e6325c7b4aa8eac3168d88d97e3208efb1
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-03 16:11:13 +0900

    new project

---
+++ src/main/java/com/swacademy/cnuworldcup/CnuWorldCupApplication.java
@@ -0,0 +1,13 @@
+package com.swacademy.cnuworldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuWorldCupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnuWorldCupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swacademy/cnuworldcup/CnuWorldCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swacademy.cnuworldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuWorldCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
