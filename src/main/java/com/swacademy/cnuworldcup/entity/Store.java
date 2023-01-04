187fd033a0c172e9f508fa0a77c4fcc198aa45f1
JeongYongCheol <dydcjf611@gmail.com>
2023-01-04 15:27:07 +0900

    dependency commit

---
+++ src/main/java/com/swacademy/cnuworldcup/entity/Store.java
@@ -0,0 +1,26 @@
+package com.swacademy.cnuworldcup.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Store {
+
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
--- src/main/java/com/swacademy/cnuworldcup/testclass.java
+++
@@ -1,6 +0,0 @@
-package com.swacademy.cnuworldcup;
-
-public class testclass {
-    public static void main(String[] args) {
-        //branch test
-}
