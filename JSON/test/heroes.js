ec1933e844bf0f669434d5a68d0c225961d25962
soyeeee2 <soyeeee2@gmail.com>
2023-01-04 14:49:24 +0900

    JSON test added

---
+++ JSON/test/heroes.js
@@ -0,0 +1,64 @@
+let testData = JSON.parse(JSON.stringify(TestFile));
+console.log(testData);
+
+var header = document.querySelector('header');
+var section = document.querySelector('section'); 
+
+var request = new XMLHttpRequest();
+
+request.open('GET', testData);
+request.responseType = 'text';
+request.send();
+
+
+request.onload = function() {
+    var superHeroesText = request.response;
+    var superHeroes = JSON.parse(superHeroesText);
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+  }
+
+
+function populateHeader(jsonObj) {
+    var myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['squadName'];
+    header.appendChild(myH1);
+  
+    var myPara = document.createElement('p');
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    header.appendChild(myPara);
+}
+
+function showHeroes(jsonObj) {
+    var heroes = jsonObj['members'];
+  
+    for (var i = 0; i < heroes.length; i++) {
+      var myArticle = document.createElement('article');
+      var myH2 = document.createElement('h2');
+      var myPara1 = document.createElement('p');
+      var myPara2 = document.createElement('p');
+      var myPara3 = document.createElement('p');
+      var myList = document.createElement('ul');
+  
+      myH2.textContent = heroes[i].name;
+      myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+      myPara2.textContent = 'Age: ' + heroes[i].age;
+      myPara3.textContent = 'Superpowers:';
+  
+      var superPowers = heroes[i].powers;
+      for (var j = 0; j < superPowers.length; j++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = superPowers[j];
+        myList.appendChild(listItem);
+      }
+  
+      myArticle.appendChild(myH2);
+      myArticle.appendChild(myPara1);
+      myArticle.appendChild(myPara2);
+      myArticle.appendChild(myPara3);
+      myArticle.appendChild(myList);
+  
+      section.appendChild(myArticle);
+    }
+  }
+  
