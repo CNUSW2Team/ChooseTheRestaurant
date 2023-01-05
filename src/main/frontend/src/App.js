555d5732819d7a62bbbca106245c7adc742d9dea
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-05 16:32:31 +0900

    frontend 프로젝트 위치 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [store, list] = useState('')
+
+  useEffect(() => {
+    axios.get('/store/list')
+        .then(response => list(response.data))
+        .catch(error => console.log(error))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>Stores</h1>
+          {<textarea rows={7} cols={20} value={JSON.stringify(store, null, 2)} readOnly={true} />}
+      </div>
   );
 }
 
