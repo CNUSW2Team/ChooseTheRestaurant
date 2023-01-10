a88e080d7d26e42bcf4b1da2ca030cf8748db28b
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-10 17:31:17 +0900

    백엔드 컨트롤러 수정, 월드컵 servlet 추가

--- src/main/java/com/swacademy/cnuworldcup/tester.java
+++ src/main/java/com/swacademy/cnuworldcup/tester.java
@@ -1,7 +1,9 @@
 package com.swacademy.cnuworldcup;
 
 import com.swacademy.cnuworldcup.Repository.StoreRepository;
+import com.swacademy.cnuworldcup.Repository.WorldCupRepository;
 import com.swacademy.cnuworldcup.entity.Store;
+import com.swacademy.cnuworldcup.entity.WorldCup;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,23 +15,14 @@ public class tester {
 
     public static void main(String[] args) {
         var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
-        var storeRepository = applicationContext.getBean(StoreRepository.class);
+        var storeRepository = applicationContext.getBean(WorldCupRepository.class);
         UUID rand = UUID.randomUUID();
-        Store store = Store.builder()
-                .store_id(UUID.randomUUID())
-                .store_name("청춘우동")
-                .address("대전광역시 유성구 궁동 410")
-                .phone_number("042-823-7234")
-                .opening_hours("금요일\t오전 11:00~오후 8:30\n" +
-                        "토요일\t오전 11:00~오후 8:30\n" +
-                        "일요일\t오전 11:00~오후 8:30\n" +
-                        "월요일\t오전 11:00~오후 8:30\n" +
-                        "화요일\t오전 11:00~오후 8:30\n" +
-                        "수요일\t오전 11:00~오후 8:30\n" +
-                        "목요일\t오전 11:00~오후 8:30")
-                .image("/resources/static/images/store_" + rand.toString() + ".jpg")
+        WorldCup worldCup = WorldCup.builder()
+                .worldcup_id(UUID.randomUUID())
+                .worldcup_name("분위기 좋은 맛집")
+                .like_num(50)
                 .build();
         //storeRepository.deleteAll();
-        storeRepository.save(store);
+        storeRepository.save(worldCup);
     }
 }
