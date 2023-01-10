cce0110013b46c17849d88e9b883b7ae97bffb1f
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-10 17:44:42 +0900

    백엔드 컨트롤러 수정, 월드컵 servlet 추가

---
+++ src/main/frontend/src/components/RankingPage.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Button, Form, Table} from "react-bootstrap";
+import {Link, useParams} from "react-router-dom";
+
+function RankingPage() {
+    const [store, setData] = useState([]);
+    let { worldcupId } = useParams();
+
+    useEffect(() => {
+            axios.get('/store/all')
+                .then(response => {
+                    setData(response.data);
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        []);
+    return (
+        <Form>
+            <h1>Store Table</h1>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>address</th>
+                    <th>phone_number</th>
+                    <th>opening_hours</th>
+                </tr>
+                </thead>
+                <tbody>
+                {store.map(v =>
+                    <tr>
+                        <td>{v.store_id}</td>
+                        <td>{v.store_name}</td>
+                        <td>{v.address}</td>
+                        <td>{v.phone_number}</td>
+                        <td>{v.opening_hours}</td>
+                        <td><Link to={`/Store/${v.store_id}`}> 이동하기 </Link></td>
+                    </tr>,
+                )}
+                </tbody>
+            </Table>
+        </Form>
+    );
+}
+
+export default RankingPage;
+
--- src/main/java/com/swacademy/cnuworldcup/tester.java
+++ src/main/java/com/swacademy/cnuworldcup/tester.java
@@ -1,7 +1,9 @@
 package com.swacademy.cnuworldcup;
 
+import com.swacademy.cnuworldcup.Repository.RelationRepository;
 import com.swacademy.cnuworldcup.Repository.StoreRepository;
 import com.swacademy.cnuworldcup.Repository.WorldCupRepository;
+import com.swacademy.cnuworldcup.entity.Relation;
 import com.swacademy.cnuworldcup.entity.Store;
 import com.swacademy.cnuworldcup.entity.WorldCup;
 import lombok.extern.slf4j.Slf4j;
@@ -15,14 +17,15 @@ public class tester {
 
     public static void main(String[] args) {
         var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
-        var storeRepository = applicationContext.getBean(WorldCupRepository.class);
+        var storeRepository = applicationContext.getBean(RelationRepository.class);
         UUID rand = UUID.randomUUID();
-        WorldCup worldCup = WorldCup.builder()
-                .worldcup_id(UUID.randomUUID())
-                .worldcup_name("분위기 좋은 맛집")
-                .like_num(50)
+        Relation relation = Relation.builder()
+                .relation_id(rand)
+                .worldcup_id(UUID.fromString("0b44cd73-6ab8-4500-9d07-fbc9cdfa03bd"))
+                .store_id(UUID.fromString("3ee3785d-afe3-46f5-999e-ae77a0dc8d06"))
+                .win_count(1)
                 .build();
         //storeRepository.deleteAll();
-        storeRepository.save(worldCup);
+        storeRepository.save(relation);
     }
 }
