3021f0e8e734424a6d39ead6df60e4736bd8a777
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-10 16:32:46 +0900

    router 테스트

--- src/main/frontend/src/components/Store.js
+++ src/main/frontend/src/components/Store.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
+import axios from "axios";
 
 function Store() {
     let { storeId } = useParams();
+
+    const [store, setData] = useState([]);
+    useEffect(() => {
+            axios.get(`/store/${storeId}`)
+                .then(response => {
+                    setData(response.data);
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        []);
     return (
         <div>
-            StoreID: {storeId}<p></p>
-            {/*StoreName: {JSON.stringify(store[0]) && JSON.stringify(store[0]["store_name"])}<p></p>*/}
+            StoreID: {JSON.stringify(store) && JSON.stringify(store['store_id'])}<p></p>
+            StoreName: {JSON.stringify(store) && JSON.stringify(store['store_name'])}<p></p>
             <img src={`/img/${storeId}.jpg`} />
         </div>
 
