51b58a95dd03959dc7fd7f6fbd13ceccd106e537
soyeeee2 <soyeeee2@gmail.com>
2023-01-12 14:40:15 +0900

    fixed

--- src/main/frontend/src/components/RoundChoice.js
+++ src/main/frontend/src/components/RoundChoice.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Button, Form, Table} from "react-bootstrap";
 import {Link, useParams} from "react-router-dom";
+import WorldCup from './WorldCup';
 
 function RoundChoice() {
     // 라운드 선택 >> 사진, 가게이름
@@ -19,16 +20,14 @@ function RoundChoice() {
                 })
         },
         []);
-    
 
-
+        
     return (
         // 팝업 & 월드컵 데이터 같이 받기
         <div>
-            <Link to={`/RoundChoice/${worldcupId}/WorldCup`}><button>Round : 2강</button></Link>
-            {JSON.stringify(category)}
-            <Link to={`/Ranking/${worldcupId}`}><button>랭킹페이지로 가기</button></Link>
-            
+            <Link to={`/RoundChoice/${worldcupId}/WorldCup`} state={{ rounds: 2 }}><button>Round : 2강</button></Link>
+            <Link to={`/RoundChoice/${worldcupId}/WorldCup`} state={{ rounds: 4 }}><button>Round : 4강</button></Link>
+            <Link to={`/Ranking/${worldcupId}`}><button>랭킹페이지로 가기</button></Link> 
         </div>
         
     );
