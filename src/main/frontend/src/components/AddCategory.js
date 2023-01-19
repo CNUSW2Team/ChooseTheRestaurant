2926184518aba776a15448d47c635df23c135034
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-19 16:03:05 +0900

    카테고리 생성페이지 추가

--- src/main/frontend/src/components/AddCategory.js
+++ src/main/frontend/src/components/AddCategory.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Checkbox} from 'antd';
-import {Link} from "react-router-dom";
+import {Link, useHref} from "react-router-dom";
 
 import {useEffect, useState} from 'react';
 import axios from "axios";
 import {Form, Table} from "react-bootstrap";
 import styles from "./AddCategory.module.css"
+
 
 function AddCategory() {
     const [store, setStore] = useState([]);
@@ -13,16 +14,25 @@ function AddCategory() {
     const updateCategoryName = e => setCategoryName(e.target.value);
     const [categoryName, setCategoryName] = useState('');
 
-    function CreateCategory(){
+    function CreateCategory() {
         const fd = new FormData();
 
-        const categoryDto = {
-            category_name: categoryName, stores: select
-        }
-        fd.append("categoryDto", JSON.stringify(categoryDto));
+        if (categoryName.length < 5) {
+            alert('월드컵 이름은 5자 이상으로 해 주세요.');
+        } else {
+            const categoryDto = {
+                category_name: categoryName, stores: select
+            }
+            fd.append("categoryDto", JSON.stringify(categoryDto));
 
-        axios.post('http://localhost:8080/requestCategoryAdd', fd)
-            .then((response) => {console.log(response.data)})
+            axios.post('http://localhost:8080/requestCategoryAdd', fd)
+                .then((response) => {
+                    console.log(response.data)
+                })
+                .then(() =>{
+                    window.location.href = "/AllCategory";
+                })
+        }
     }
 
     const onChangeAll = (e) => {
@@ -57,21 +67,27 @@ function AddCategory() {
 
     return (
         <Form>
-            <h1>월드컵 이름: <input onChange={updateCategoryName}/> </h1>
+            <h1>월드컵 이름: <input onChange={updateCategoryName}/></h1>
+            <button type={"button"} onClick={() => CreateCategory()}>등록하기</button>
             <Form className={styles.left}>
                 <h1>Selected Store Table</h1>
-                <Table striped bordered hover>
+                <Table className={styles.table}>
                     <thead>
                     <tr>
+                        <th>선택 해제</th>
                         <th>name</th>
                         <th>address</th>
+                        <th>id</th>
                     </tr>
                     </thead>
                     <tbody>
-                    {store.filter(v =>  select.includes(v.store_id)).map(v =>
+                    {store.filter(v => select.includes(v.store_id)).map(v =>
                         <tr>
+                            <td><Checkbox onChange={(e) => onChangeEach(e, v.store_id)}
+                                          checked={select.includes(v.store_id)}/></td>
                             <td>{v.store_name}</td>
                             <td>{v.address}</td>
+                            <td>{v.store_id}</td>
                         </tr>,
                     )}
                     </tbody>
@@ -90,24 +106,23 @@ function AddCategory() {
                         <th>선택</th>
                         <th>name</th>
                         <th>address</th>
-                        <th>id</th>
+                        <th>상세정보</th>
                     </tr>
                     </thead>
                     <tbody>
-                    {store.map(v =>
-                        <tr>
-                            <Checkbox onChange={(e) => onChangeEach(e, v.store_id)}
-                                      checked={select.includes(v.store_id)}/>
+                    {store.filter(v => !select.includes(v.store_id)).map(v =>
+                        <tr key={v.store_id}>
+                            <td><Checkbox onChange={(e) => onChangeEach(e, v.store_id)}
+                                          checked={select.includes(v.store_id)}/></td>
                             <td>{v.store_name}</td>
                             <td>{v.address}</td>
-                            <td>{v.store_id}</td>
-                            {/*<td><Link to={`/Store/${v.store_id}`}> 이동하기 </Link></td>*/}
+                            {/*<td>{v.store_id}</td>*/}
+                            <td><Link to={`/Store/${v.store_id}`}> 상세정보 </Link></td>
                         </tr>,
                     )}
                     </tbody>
                 </Table>
             </Form>
-            <button onClick={() => CreateCategory()}>등록하기</button>
         </Form>
     );
 }
