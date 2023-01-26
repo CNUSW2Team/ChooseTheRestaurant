3c65097c6ab6b0632d922806adac62129cd705eb
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-26 18:34:48 +0900

    카드뷰, 테이블 css 추가

--- src/main/frontend/src/components/admin/AdminAddMenuToStore.js
+++ src/main/frontend/src/components/admin/AdminAddMenuToStore.js
@@ -1,16 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Checkbox} from "antd";
 import {Table} from "react-bootstrap";
+
 function AdminAddMenuToStore(props) {
 
     const [store, setStore] = useState([]);
     const [storeId, setStoreId] = useState("");
+    const [storeName, setStoreName] = useState("");
     const [menu, setMenu] = useState([]);
     const [isMenuLoaded, setIsMenuLoaded] = useState(0);
 
-    const updateStoreId = e => {setStoreId(e.target.value)};
+    const [menuName, setMenuName] = useState([]);
+    const [price, setPrice] = useState([]);
+    const [file, setFile] = useState(null);
 
+    const updateStoreId = e => {
+        setStoreId(e.target.value)
+    };
+
+    function updateStoreInfo(e, storeName) {
+        setStoreId(e.target.value);
+        setStoreName(storeName);
+    }
+
+    const updateMenuName = e => setMenuName(menuName => [...menuName, e.target.value]);
+    const updatePrice = e => setPrice(price => [...price, e.target.value]);
+    const handleChangeFile = e => setFile(e.target.files);
 
     useEffect(() => {
             axios.get('/AllStore')
@@ -39,7 +54,8 @@ function AdminAddMenuToStore(props) {
                     <tbody>
                     {store.map(s => (
                         <tr key={s.store_id}>
-                            <td><input type={"radio"} name={"selectedStore"} value={s.store_id} onChange={updateStoreId}/></td>
+                            <td><input type={"radio"} name={"selectedStore"} value={s.store_id}
+                                       onChange={e => updateStoreInfo(e, s.store_name)}/></td>
                             <td>{s.store_name}</td>
                             <td>{s.address}</td>
                         </tr>
@@ -52,12 +68,13 @@ function AdminAddMenuToStore(props) {
 
     function showStoreList() {
         setStoreId("");
+        setStoreName("");
         setMenu([]);
         setIsMenuLoaded(0);
     }
 
     function printStoreMenuList() {
-        if(!menu.length && !isMenuLoaded) {
+        if (!menu.length && !isMenuLoaded) {
             axios.get(`http://localhost:8080/StoreMenusInfo/${storeId}`)
                 .then(response => {
                     setMenu(response.data);
@@ -68,47 +85,82 @@ function AdminAddMenuToStore(props) {
                     console.log(error);
                 })
         }
-        if(isMenuLoaded && !menu.length)
+        if (isMenuLoaded && !menu.length)
             return (
                 <h3>선택한 가게에 메뉴가 존재하지 않습니다.</h3>
             );
-        
-        return (
-          <div>
-              <div>
-                  <Table id={"menuTable"}>
-                      <thead>
-                      <tr>
-                          <th>선택</th>
-                          <th>메뉴</th>
-                          <th>가격</th>
-                          <th>이미지</th>
-                      </tr>
-                      </thead>
-                      <tbody>
-                      {menu.map(m => (
-                          <tr key={m.menu_id}>
-                              <td><input type={"checkbox"} name={"selectedMenu"} value={m.menu_id}/></td>
-                              <td>{m.menu_name}</td>
-                              <td>{m.price}</td>
-                              <td><img width={200} height={100} src={`/img/${m.menu_id}.jpg`}/></td>
-                          </tr>
-                      ))}
-                      </tbody>
-                  </Table>
-              </div>
 
-          </div>
+        return (
+            <div>
+                <h2>{storeName} 메뉴</h2>
+                <Table id={"menuTable"}>
+                    <thead>
+                    <tr>
+                        <th>선택</th>
+                        <th>메뉴</th>
+                        <th>가격</th>
+                        <th>이미지</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {menu.map(m => (
+                        <tr key={m.menu_id}>
+                            <td><input type={"checkbox"} name={"selectedMenu"} value={m.menu_id}/></td>
+                            <td>{m.menu_name}</td>
+                            <td>{m.price}</td>
+                            <td><img width={200} height={100} src={`/img/${m.menu_id}.jpg`}/></td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </Table>
+            </div>
         );
     }
 
+    function formAddMenu() {
+        return (
+            <div style={{
+                border: "1px solid black",
+                backgroundColor: "white",
+                padding: "20px 20px 20px 20px",
+                overflow: "hidden"
+            }}>
+                <h2>메뉴 추가하기</h2>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>메뉴 이름 :</td>
+                        <td><input onChange={updateMenuName}/></td>
+                    </tr>
+                    <tr>
+                        <td>가격 :</td>
+                        <td><input onChange={updatePrice}/></td>
+                    </tr>
+                    <tr>
+                        <td><input type={"file"} onChange={handleChangeFile}/></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button onClick={() => addMenu()}>메뉴 추가하기</button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 
+    function addMenu() {
+        console.log(file[0]);
+    }
 
     return (
         <div>
             <h1>Hello Menu</h1>
             {storeId ? <button onClick={showStoreList}>가게 변경</button> : printStoreList()}
             {!storeId ? <h2>가게를 선택해주세요</h2> : printStoreMenuList()}
+            {storeId ? formAddMenu() : ""}
+            {}
         </div>
     );
 }
