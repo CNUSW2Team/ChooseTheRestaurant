c30ccbd84d2aeb5c87a085267a03157458364eb9
JunyeongKim <wkrwjafl15@gmail.com>
2023-01-30 18:12:23 +0900

    이미지 불러오기 수정

---
+++ src/main/frontend/src/components/Menus.js
@@ -0,0 +1,23 @@
+import styles from "./table.module.css"
+
+function Menus(props) {
+    return (
+        <table className={styles.table}>
+            <thead className={styles.thead}>
+            <tr>
+                <th className={styles.th}>메뉴명</th>
+                <th className={styles.th}>가격</th>
+            </tr>
+            </thead>
+            <tbody>
+            {props.menus && props.menus.map(v => <tr>
+                <td className={styles.td}>{v.menu_name}</td>
+                <td className={styles.td}>{v.price}</td>
+            </tr>,)}
+            </tbody>
+        </table>
+);
+}
+
+export default Menus;
+
