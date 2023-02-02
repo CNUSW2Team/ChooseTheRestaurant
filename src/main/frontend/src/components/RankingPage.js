f3d8b6d1a3468c6c110b62cadc65b902ff083969
JeongYongCheol <dydcjf611@gmail.com>
2023-02-02 17:03:29 +0900

    랭킹페이지 살짝 바꿈

--- src/main/frontend/src/components/RankingPage.js
+++ src/main/frontend/src/components/RankingPage.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import {Button, Form, Table} from "react-bootstrap";
 import {Link, useParams} from "react-router-dom";
 import styles from "./RankingPage.module.css"
-import KakaoMap from "./kakao/KakaoMap";
 import RankingTable from "./worldcup/RankingTable";
 
 function RankingPage() {
@@ -13,17 +12,6 @@ function RankingPage() {
     const [category, setCategory] = useState([]);
     let { categoryId } = useParams();
 
-    // useEffect(() => {
-    //         axios.get(`/Ranking/${categoryId}`)
-    //             .then(response => {
-    //                 setStore(response.data);
-    //                 console.log(response.data);
-    //             })
-    //             .catch(error => {
-    //                 console.log(error);
-    //             })
-    //     },
-    //     []);
     useEffect(() => {
 
             axios.get(`/AllCategory/${categoryId}`)
@@ -37,21 +25,28 @@ function RankingPage() {
         },
         []);
 
-
-    let rankingCount = 0;
-
     return (
         <div className="wrapper">
             <div className="main">
                 <h1><p>{category && category.category_name} 월드컵 순위</p></h1>
                 <div style={{display:"flex"}}>
-                        <div>
-                            <div className={styles.top3}>
-                                {store[1] && <img src={`/image/${store[1].store_id}`}/>}
-                                {store[0] && <img src={`/image/${store[0].store_id}`}/>}
-                                {store[2] && <img src={`/image/${store[2].store_id}`}/>}
-                            </div>
-                        </div>
+                    <div className={styles.top3}>
+                        {/*<div style={{float:"left"}}>*/}
+                        {/*    <p>Top2!</p>*/}
+                        {/*    {store[1] && <img src={`/image/${store[1].store_id}`}/>}*/}
+                        {/*</div>*/}
+                        {/*<div style={{float:"left"}}>*/}
+                        {/*    <p>Top1!</p>*/}
+                        {/*    {store[0] && <img src={`/image/${store[0].store_id}`}/>}*/}
+                        {/*</div>*/}
+                        {/*<div style={{float:"left"}}>*/}
+                        {/*    <p>Top3!</p>*/}
+                        {/*    {store[2] && <img src={`/image/${store[2].store_id}`}/>}*/}
+                        {/*</div>*/}
+                        {store[1] && <img src={`/image/${store[1].store_id}`}/>}
+                        {store[0] && <img src={`/image/${store[0].store_id}`}/>}
+                        {store[2] && <img src={`/image/${store[2].store_id}`}/>}
+                    </div>
                         <RankingTable category={categoryId} setStore={setStore}/>
                     </div>
             </div>
