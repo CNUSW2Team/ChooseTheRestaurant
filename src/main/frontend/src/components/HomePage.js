720687ebdb927513524fe9d321d5ca6418243eeb
soyeeee2 <soyeeee2@gmail.com>
2023-03-03 19:47:46 +0900

    test

--- src/main/frontend/src/components/HomePage.js
+++ src/main/frontend/src/components/HomePage.js
@@ -1,10 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
-import {adminBtn, adminGet} from "./auth/AdminUtil";
 
 function HomePage() {
-
-
     return (
         <div className='min-vh-100' style={{backgroundColor:"#E7DDF7"}}>
             <main className="position-absolute top-50 start-50 translate-middle text-center">
@@ -13,12 +10,12 @@ function HomePage() {
                 <p className="lead"> 다른 사람들은 어떤 가게를 선호하는지 궁금하신가요? </p>
                 <p className="lead"> 충남대 근처의 가게들로 구성된 맛집 월드컵을 통해 확인해보세요! </p>
                 <div>
+                    {/* test */}
                     <a href="/AllCategory" className="btn btn-lg btn-secondary fw-bold border-white ">월드컵</a>
-                    <a href="/AllMenu" className="btn btn-lg btn-secondary fw-bold border-white ">태그검색</a>
-                    <a href="/Roulette" className="btn btn-lg btn-secondary fw-bold border-white ">룰렛</a>
+                    <a href="/AllCategory" className="btn btn-lg btn-secondary fw-bold border-white ">태그검색</a>
+                    <a href="/AllCategory" className="btn btn-lg btn-secondary fw-bold border-white ">룰렛</a>
                     <a href="/AllCategory" className="btn btn-lg btn-secondary fw-bold border-white ">사다리</a>
                 </div>
-
             </main>
         </div>
     );
