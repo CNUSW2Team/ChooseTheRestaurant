5d703661aaa03b75bb0e6043be8fc6e30ca30d42
soyeeee2 <92347149+soyeeee2@users.noreply.github.com>
2023-07-19 10:33:41 +0900

    Revert "더미추가"

--- src/main/frontend/src/components/worldcup/AllCategory.js
+++ src/main/frontend/src/components/worldcup/AllCategory.js
@@ -2,25 +2,23 @@ import React, {useEffect, useRef, useState} from 'react';
 import axios from "axios";
 import {createFuzzyMatcher} from "../../util/util";
 import StartButton from "./StartButton";
-import categories_dummy from '../dummyData/categories.json'
-import dummy_img from '../dummyImg/카테고리더미.jpg'
 
 function AllCategory() {
     const [searchBox, setSearchBox] = useState('');
-    const [category, setCategory] = useState(categories_dummy);
+    const [category, setCategory] = useState([]);
     const updateSearchBox = e => setSearchBox(e.target.value);
     const [sort, setSort] = useState('인기순');
 
-    // useEffect(() => {
-    //     axios.get('/api/Category')
-    //         .then(response => {
-    //             setCategory(response.data);
-    //             // console.log("initial:", response.data);
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         })
-    // }, []);
+    useEffect(() => {
+        axios.get('/api/Category')
+            .then(response => {
+                setCategory(response.data);
+                // console.log("initial:", response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
 
     function updateSort(value){
         setSort(value);
@@ -96,8 +94,7 @@ function AllCategory() {
                 {category.filter(v => createFuzzyMatcher(searchBox).test(v.category_name.toLowerCase())).map(v =>
                     <div className="col" key={v.category_id}>
                         <div className="card shadow">
-                            {/* <img src={`/image/${v.category_id}`} className="card-img-top " alt="..."/> */}
-                            <img src={dummy_img} />
+                            <img src={`/image/${v.category_id}`} className="card-img-top " alt="..."/>
                             <div className="text-end bg-danger text-white pe-3 py-1">
                                 ♥ {v.favorite}
                             </div>
